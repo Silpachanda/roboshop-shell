12a2efcd833664b0f51c754fee731a430ca229a6
shilpachanda <silpa.chanda@gmail.com>
2023-05-03 21:50:58 +0530

    added rm command for removing the existing content in /app directory

--- redis.sh
+++ redis.sh
@@ -2,6 +2,7 @@ echo -e "\e[36m>>>>Install redis repo<<<<\e]0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
 echo -e "\e[36m>>>>enable redis repo<<<<\e]0m"
 dnf module enable redis:remi-6.2 -y
 
