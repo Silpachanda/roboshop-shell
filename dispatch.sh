88708ba0341283ee8f09143ea3fe0b0fd6c0e6c5
shilpachanda <silpa.chanda@gmail.com>
2023-05-03 23:23:32 +0530

    added echo commands and rm and restart

--- dispatch.sh
+++ dispatch.sh
@@ -1,14 +1,23 @@
+echo -e "\e[36m>>>>Install golang<<<<\e]0m"
 yum install golang -y
+echo -e "\e[36m>>>>add user<<<<\e]0m"
 useradd roboshop
+echo -e "\e[36m>>>>remove content<<<<\e]0m"
+rm -rf /app
 mkdir /app
+echo -e "\e[36m>>>>Download dispatch<<<<\e]0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+echo -e "\e[36m>>>>unzip dispatch<<<<\e]0m"
 cd /app
 unzip /tmp/dispatch.zip
 cd /app
+echo -e "\e[36m>>>>dispatch details<<<<\e]0m"
 go mod init dispatch
 go get
 go build
+echo -e "\e[36m>>>>copy contents<<<<\e]0m"
 cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[36m>>>>restart the service<<<<\e]0m"
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
